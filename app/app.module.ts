69167ff8acce10f60f03615197e1fcb83a565682
Marcos Almeida <marcos.almeida@abacomm.com.br>
2017-03-04 12:14:01 -0300

    Criando módulo de contatos.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { ContatosModule } from './contatos/contatos.module';
+
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        ContatosModule
+    ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
