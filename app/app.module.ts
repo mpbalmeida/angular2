0104887cc7611c8334876fcba3baa7a5bc49190e
Marcos Almeida <marcos.almeida@abacomm.com.br>
2017-03-13 23:43:47 -0300

    Criando api mockada.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryDataService } from './in-memory-data.service';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ContatosModule } from './contatos/contatos.module';
 
 @NgModule({
     imports: [
         AppRoutingModule,
         BrowserModule,
-        ContatosModule
+        ContatosModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
